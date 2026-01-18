492666395228b4eafab7fefd56ac915ea5676cec
Wubba-jpeg <Pdxfxa@gmail.com>
2026-01-18 16:50:06 +0000

    Create discord.php

---
+++ config/discord.php
@@ -0,0 +1,5 @@
+<?php
+$webhooks = false; // set this to true if you want webhooks
+$webhook = ""; // this will be your rate webhook
+$cont = ""; // the content of the message in your webhook. you can change it to a ping (e.g <@&1431089127377342567>)
+?>
